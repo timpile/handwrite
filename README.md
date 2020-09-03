6cff9f022ac19ba7f970fe422a4a2dde35b18373
Tim Pile <timspile@gmail.com>
2020-09-03 18:35:06 -0400

    Update readme and mix package details

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Handwrite
 
-An Elixir library for working with [Handwrite.io](https://documentation.handwrite.io/)
+An unofficial Elixir library for working with [Handwrite.io](https://documentation.handwrite.io/)
+
+## Documentation
+
+Once published, the docs can be found at [https://hexdocs.pm/handwrite](https://hexdocs.pm/handwrite).
 
 ## Installation
 
@@ -24,8 +28,3 @@ use Mix.Config
 
 config :handwrite, api_key: System.get_env("HANDWRITE_API_KEY")
 ```
-
-Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
-and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
-be found at [https://hexdocs.pm/handwrite](https://hexdocs.pm/handwrite).
-
--- mix.exs
+++ mix.exs
@@ -7,14 +7,13 @@ defmodule Handwrite.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps(),
       name: "Handwrite",
       source_url: "https://github.com/timpile/handwrite",
       homepage_url: "https://github.com/timpile/handwrite",
-      docs: [
-        main: "Handwrite",
-        extras: ["README.md"]
-      ]
+      docs: docs()
     ]
   end
 
@@ -29,6 +28,25 @@ defmodule Handwrite.MixProject do
       {:httpoison, "~> 1.6"},
       {:poison, "~> 4.0"},
       {:ex_doc, "~> 0.22", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description() do
+    "An unofficial Elixir library for working with the Handwrite.io API."
+  end
+
+  defp package() do
+    [
+      files: ~w(lib test .formatter.exs mix.exs README.md),
+      licenses: [],
+      links: %{"GitHub" => "https://github.com/timpile/handwrite"}
+    ]
+  end
+
+  defp docs() do
+    [
+      main: "Handwrite",
+      extras: ["README.md"]
     ]
   end
 end
